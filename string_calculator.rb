41c8b78580d4656fcc805b0910c158ec11166f95
lalit hammad <hammadlalit8@gmail.com>
2025-06-13 12:17:56 +0530

    first step for writing test cases and code for  passing the cases

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,7 @@
 class StringCalculator
     def add(input)
         return 0 if input.empty?
+        array_of_inputs = input.split(",").map{|x| x.to_i}
+        sum = array_of_inputs.sum
     end
 end
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -6,4 +6,30 @@ RSpec.describe StringCalculator do
     it "returns 0 for an empty string" do 
         expect(calc.add("")).to eq(0)
     end
+    context "single number" do  
+        it "returns 1 for a string '1'" do 
+            expect(calc.add("1")).to eq(1)
+        end
+        it "returns 3 for a string '3'" do 
+            expect(calc.add("3")).to eq(3)
+        end
+    end
+    context "duble digit numbers" do 
+        it "return 4 for string '1,3'" do 
+            expect(calc.add("1,3")).to eq(4)
+        end
+
+        it "return 10 for string '7,3'" do 
+            expect(calc.add("7,3")).to eq(10)
+        end
+    end
+    context "three digit numbers" do 
+        it "return 7 for string '1,2,4'" do 
+            expect(calc.add("1,2,4")).to eq(7)
+        end
+
+        it "return 12 for string '7,3,2'" do 
+            expect(calc.add("7,3,2")).to eq(12)
+        end
+    end
 end
